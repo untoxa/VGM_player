fbb83902ea2554e4e3dca61a8363e9fb989f315e
Toxa <untoxa@mail.ru>
2024-09-28 18:16:38 +0300

    VGM 50/60Hz rate compensation for the SMS/GG

--- src/gg/vgm_player.c
+++ src/gg/vgm_player.c
@@ -43,7 +43,7 @@ void vgm_play_cut(void) {
     }
 }
 
-void vgm_play_isr(void) {
+void vgm_play(void) {
     static uint16_t delay;
     static uint16_t start, stop;
 
@@ -112,9 +112,42 @@ void vgm_play_isr(void) {
     }
 }
 
+#define NTSC_HBLANK_PERIOD 48
+#define NTSC_CORRECTION_PERIOD 5
+#define NTSC_60HZ_COUNTER 5
+#define NTSC_50HZ_COUNTER 6
+
+#define PAL_HBLANK_PERIOD 39
+#define PAL_CORRECTION_PERIOD 1
+#define PAL_60HZ_COUNTER 5
+#define PAL_50HZ_COUNTER 6
+
+bool vblank_triggered = false;
+uint8_t interrupt_counter = 0, interrupt_max_counter = NTSC_50HZ_COUNTER, scanline_counter = NTSC_HBLANK_PERIOD, scanline_correction = NTSC_CORRECTION_PERIOD;
+void vblank_isr(void) {
+    __WRITE_VDP_REG(VDP_R10, scanline_correction);
+    vblank_triggered = true;
+    if (++interrupt_counter == interrupt_max_counter) {
+        vgm_play();
+        interrupt_counter = 0;
+    }
+}
+void scanline_isr(void) {
+    if (vblank_triggered) {
+        __WRITE_VDP_REG(VDP_R10, scanline_counter); // set scanline counter to the target value
+        VCOUNTER = 0xff;                            // retrigger the scanline counter
+        vblank_triggered = false;
+    } else {
+        if (++interrupt_counter == interrupt_max_counter) {
+            vgm_play();
+            interrupt_counter = 0;
+        }
+    }
+}
 
 VGM_RESULT vgm_play_file(const uint8_t * name) {
     static uint32_t temp[5];
+    static uint32_t rate;
     static uint32_t data_offset;
     static uint16_t bytes_loaded;
     static uint16_t load;
@@ -137,6 +170,9 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
     // check PSG || FM
     if ((temp[3] == 0) && (temp[4] == 0)) return VGM_UNSUPORTED_CHIP;
 
+    pf_lseek(0x24);
+    if (pf_read(&rate, sizeof(rate), &bytes_loaded) != FR_OK) return VGM_READ_ERROR;
+
     // locate sound data block offset
     if (temp[2] >= VGM_VERSION_1_50) {
         pf_lseek(0x34);
@@ -153,10 +189,27 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
     isr_result = VGM_OK;
     isr_skip = 0;
 
+    if (get_system() == SYSTEM_60HZ) {
+        scanline_counter = NTSC_HBLANK_PERIOD;
+        scanline_correction = NTSC_CORRECTION_PERIOD;
+        interrupt_max_counter = ((uint8_t)rate == 50) ? NTSC_50HZ_COUNTER : NTSC_60HZ_COUNTER;
+    } else {
+        scanline_counter = PAL_HBLANK_PERIOD;
+        scanline_correction = PAL_CORRECTION_PERIOD;
+        interrupt_max_counter = ((uint8_t)rate == 50) ? PAL_50HZ_COUNTER : PAL_60HZ_COUNTER;
+    }
+
+    // set the correct settings for the PAL/NTSC compensation
+    __WRITE_VDP_REG(VDP_R10, 0x00);
+
     disable_interrupts();
-    remove_VBL(vgm_play_isr);
-    add_VBL(vgm_play_isr);
+    remove_VBL(vblank_isr);
+    add_VBL(vblank_isr);
+    remove_LCD(scanline_isr);
+    add_LCD(scanline_isr);
     enable_interrupts();
+
+    set_interrupts(VBL_IFLAG | LCD_IFLAG);
 
     while (true) {
         PROCESS_INPUT();
@@ -193,8 +246,11 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
     }
 
     disable_interrupts();
-    remove_VBL(vgm_play_isr);
+    remove_VBL(vblank_isr);
+    remove_LCD(scanline_isr);
     enable_interrupts();
+
+    set_interrupts(VBL_IFLAG);
 
     vgm_play_cut();
     return (isr_result == VGM_EOF) ? VGM_OK : isr_result;
