4be79ee4957f876d5a5365a503dd92b506b273fc
Toxa <untoxa@mail.ru>
2024-11-17 21:23:38 +0300

    fix buffer overrun on the game boy on some complex VGM's

--- src/gb/vgm_player.c
+++ src/gb/vgm_player.c
@@ -9,7 +9,7 @@
 
 #define TICKS_PER_FRAME 735
 
-#define PLAY_BUFFER_SIZE 128
+#define PLAY_BUFFER_SIZE 256
 
 uint8_t last_vgm_command;
 
@@ -20,11 +20,7 @@ uint8_t *play_load;
 
 uint8_t load_buffer[LOAD_BUFFER_SIZE];
 uint8_t * load_ptr;
-#if LOAD_BUFFER_SIZE > 255
 uint16_t bytes_loaded;
-#else
-uint8_t bytes_loaded;
-#endif
 
 inline void read_init(void) {
     load_ptr = load_buffer; bytes_loaded = 0;
@@ -43,18 +39,7 @@ inline void vgm_play_cut(void) {
     NR52_REG = 0;
 }
 
-#if 0
-static void vgm_play_buffer(uint8_t count) {
-    uint8_t addr;
-    uint16_t *ptr = play_buffer;
-
-    for (; count; count -= 2) {
-        addr = *ptr++;
-        *((volatile uint8_t*) (0xFF00 | addr)) = *ptr++;
-    }
-}
-#else
-static void vgm_play_buffer(uint8_t count) PRESERVES_REGS(d, e) NAKED {
+static uint8_t * vgm_play_buffer(uint8_t count) PRESERVES_REGS(d, e) NAKED {
     count;
     __asm
         srl a
@@ -69,10 +54,10 @@ static void vgm_play_buffer(uint8_t count) PRESERVES_REGS(d, e) NAKED {
         dec b
         jr nz, 1$
 
+        ld bc, #_play_buffer
         ret
     __endasm;
 }
-#endif
 
 VGM_RESULT vgm_play_file(const uint8_t * name) {
     static uint32_t temp[3];
@@ -123,6 +108,9 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
                 } else {
                     read_byte();
                 }
+                if ((play_load - play_buffer) > (sizeof(play_buffer) - 4)) {
+                    play_load = vgm_play_buffer(play_load - play_buffer);
+                }
                 break;
             case 0x61:
                 delay = (uint16_t)read_byte() | (((uint16_t)read_byte()) << 8);
@@ -130,8 +118,7 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
             case 0x62:
             case 0x63:
                 vsync();
-                vgm_play_buffer(play_load - play_buffer);
-                play_load = play_buffer;
+                play_load = vgm_play_buffer(play_load - play_buffer);
                 PROCESS_INPUT();
                 if (KEY_PRESSED(J_A | J_B)) {
                     vgm_play_cut();
@@ -142,8 +129,7 @@ VGM_RESULT vgm_play_file(const uint8_t * name) {
             default:
                 // skip unsupported 0x7x waiting commands without breaking
                 if ((last_vgm_command > 0x6f) && (last_vgm_command < 0x80)) {
-                    vgm_play_buffer(play_load - play_buffer);
-                    play_load = play_buffer;
+                    play_load = vgm_play_buffer(play_load - play_buffer);
                     break;
                 }
                 // break on unsupported commands
